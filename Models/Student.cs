115c2d6af08d1ac370015e150985e7d2f38699dc
Denisio Rodrigues <denisio@ymail.com>
2024-12-19 16:25:23 -0300

    Primeiro commit

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchollApi;
+
+[Table("students")]
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(100)]
+    public string Email { get; set; }
+    
+    [Required]
+    public int Age { get; set; }
+}
